5b4656730e941b1d057252fa4edbe75984eccf29
Barak Kleyman <kleyman.barak@proton.me>
2024-02-15 21:10:08 +0200

    Still didn't add comment, B'H will add tommorow morning

--- backend/userapi/admin.py
+++ backend/userapi/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import FileModel
+
 
 # Register your models here.
+
+class FileModelAdmin(admin.ModelAdmin):
+    # the order of display on the admin pannel
+    list_display = ('file_name', 'id', 'ver', 'time_stamp')
+
+
+    # django magic, automatically adds these search fields and filter 
+    # to admin view
+    search_fields = ('file_name',) 
+    list_filter = ('ver',)
+
+admin.site.register(FileModel, FileModelAdmin)
--- backend/userapi/views.py
+++ backend/userapi/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
+from django.views import View
+from django.http import FileResponse, JsonResponse
+from django.core.serializers import serialize
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from .models import FileModel
+import json
 
 # Create your views here.
 
@@ -7,6 +14,29 @@ class HomeApiView(TemplateView):
     template_name = "home.html"
 
 
+class FileDownloadView(View):
+    def get(self, request, pk):
+        file_model = get_object_or_404(FileModel, pk=pk)
+        file_path = file_model.file.path
+        response = FileResponse(open(file_path, 'rb'))
+        response['Content-Disposition'] = f'attachment; filename="{file_model.file.name}"'
+        return response
     
-    
-    
+
+class FetchFileDataView(View):
+    def get(self, request, *args, **kwargs):
+        try:    
+            all_files_tables = FileModel.objects.all()
+
+            serialized_file_tables = [ {'file_name': table.file_name,
+                                        'ver': table.ver,
+                                        'file': 
+                                            f'{
+                                                reverse('userapi:get_with_pk',
+                                                         kwargs={'pk': table.pk})
+                                                }'} for table in all_files_tables]
+
+            return JsonResponse({'tables': serialized_file_tables})
+        
+        except Exception as e:
+            return JsonResponse({'error': 'Internal Server Error'}, status=500)
